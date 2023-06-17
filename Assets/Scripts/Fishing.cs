3d7fca6863cf17dae587a24af89991c585b357b4
Kyle-Willemsen <113157298+Kyle-Willemsen@users.noreply.github.com>
2023-06-17 19:23:38 +1000

    Fishing mechanics implemented.

---
+++ Assets/Scripts/Fishing.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fishing : MonoBehaviour
+{
+    private Animator anim;
+    public int minCatch;
+    public int maxCatch;
+
+    public float timeToCatchFish;
+    public float timer;
+    public bool hasCastLine;
+    public List<GameObject> fishList = new List<GameObject>();
+    public Transform fishSpawn;
+    public GameObject player;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1) && timer > 0 && hasCastLine)
+        {
+            ReelInFish();
+        }
+
+         else if (timer <= 0 && hasCastLine)
+         {
+             ReturnToNormal();
+         }      
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetBool("CastLine", true);
+            //StartCoroutine("CaughtFish", Random.Range(minCatch, maxCatch));
+            Invoke("CaughtFish", Random.Range(minCatch,maxCatch));
+        }
+    }
+   private void CaughtFish()
+   {
+        anim.SetBool("CaughtFish", true);
+        timer = timeToCatchFish;
+        hasCastLine = true;
+   }
+
+
+    private void ReelInFish()
+    {
+        anim.SetBool("CaughtFish", false);
+        anim.SetBool("CastLine", false);
+        Instantiate(fishList[Random.Range(0, fishList.Count)], fishSpawn.position, player.transform.rotation, player.transform);
+        hasCastLine = false;
+        Debug.Log("Caught");
+    }
+
+
+    private void ReturnToNormal()
+    {
+        anim.SetBool("CastLine", false);
+        anim.SetBool("CaughtFish", false);
+        hasCastLine = false;
+        Debug.Log("Miss");
+    }
+
+}
--- Assets/Scripts/NPC_Dialogue.cs
+++ Assets/Scripts/NPC_Dialogue.cs
@@ -13,12 +13,46 @@ public class NPC_Dialogue : MonoBehaviour
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] GameObject triggerText;
     [SerializeField] GameObject fishingRod;
+    [SerializeField] GameObject dialogueButton;
+    [SerializeField] GameObject finishButton;
 
+    public float dialogueCounter;
 
     private void Start()
     {
         mouse = GameObject.Find("Player").GetComponentInChildren<Mouse>();
     }
+
+    private void Update()
+    {
+        if (dialogueCounter == 0)
+        {
+            text.text = "Hello Traveller...";
+        }
+        if (dialogueCounter == 1)
+        {
+            text.text = "Glad to see you as I am in need of some help...";
+        }
+        if (dialogueCounter == 2)
+        {
+            text.text = "I need some fish to feed my family, however I have a terrible fear of water...";
+        }
+        if (dialogueCounter == 3)
+        {
+            text.text = "If you can catch me one each of these 5 fish, I will reward you";
+        }
+        if (dialogueCounter == 4)
+        {
+            text.text = "Will you help me?";
+        }
+        if (dialogueCounter == 5)
+        {
+            text.text = "Great!, My fishing rod os over by the pier... goodluck!";
+            dialogueButton.SetActive(false);
+            finishButton.SetActive(true);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         triggerText.SetActive(true);
@@ -48,6 +82,15 @@ public class NPC_Dialogue : MonoBehaviour
         }
     }
 
+    public void NextDialogue()
+    {
+        dialogueCounter++;
+    }
+
+    public void PreviousDialogue()
+    {
+        dialogueCounter--;
+    }
     public void ActivateObjective()
     {
         objective.SetActive(true);
